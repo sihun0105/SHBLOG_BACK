7c7b83bfd131683e3f80f756c2640c752bf1a524
sihun <thrchtlgns@naver.com>
2023-01-14 18:43:02 +0900

    [feat] 랜덤문자 변경

--- src/sendmail/sendmail.service.ts
+++ src/sendmail/sendmail.service.ts
@@ -9,8 +9,16 @@ export class SendmailService {
   ) {}
   
  
+  public async randommaker(lenth:number){
+    let text = "";
+    const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    for( var i=0; i < lenth; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    return text;
+  }
+
  public async sendMail(toEmail: string) {
-    const randomNumber: number = Math.random() * 101;
+    const randomNumber = await this.randommaker(6);
     console.log('randomNumber: ', randomNumber);
     const value = await this.cacheManager.set(`${toEmail}`, randomNumber,60);
     const returnValue = await this.cacheManager.get(`${toEmail}`);
