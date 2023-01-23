874b5a81cea7cfa4bd5f18a8c37a236be91f22aa
sihun <thrchtlgns@naver.com>
2023-01-23 16:56:54 +0900

    feat 엔티티 명 변경

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,6 +1,6 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
 import dotenv from 'dotenv';
-import { User } from 'src/entities/user.entity';
+import { Users } from 'src/entities/user.entity';
 
 dotenv.config();
 const config: TypeOrmModule = {
@@ -10,7 +10,7 @@ const config: TypeOrmModule = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  entities : [User],
+  entities : [Users],
   autoLoadEntities: true,
   charset: 'utf8mb4',
   synchronize: false, //개발환경일 때 만 한번만 돌리고 false해야댐 아니면 DB다시 만들어서 데이터 날라감 주의!!!!
