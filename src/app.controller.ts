4a5cf776a1ff6ee185b926affaf87c8c23ad27f6
sihun <thrchtlgns@naver.com>
2023-01-23 16:58:22 +0900

    fix app컨트롤러 수정

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,12 @@
 import { Controller, Request, Post, UseGuards } from '@nestjs/common';
-import { LocalAuthGuard } from './auth/local-auth.guard';
-import { AuthService } from './auth/auth.service';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private authService: AuthService) {}
+  constructor(private appservice: AppService) {}
 
-  @UseGuards(LocalAuthGuard)
-  @Post('auth/login')
-  async login(@Request() req) {
-    return this.authService.login(req.user);
+  @Post('/')
+  async getHello() {
+    return this.appservice.getHello();
   }
 }
