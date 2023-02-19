8915abee0fe59c474e7350577cda7347631703a7
sihun <thrchtlgns@naver.com>
2023-02-19 17:03:16 +0900

    fix 가입유저 dto수정

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,7 +1,31 @@
 import { ApiProperty, PickType } from '@nestjs/swagger';
 import { Users } from '../../entities/user.entity';
-export class CreateUserDto extends PickType(Users, [
-  'email',
-  'nickname',
-  'password',
-]) {}
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+export class CreateUserDto {
+  @ApiProperty({
+  example : "test@test.com",
+  description : "가입할 이메일",
+  required : true
+  })
+  @IsEmail()
+  @IsNotEmpty()
+  @IsString()
+  email : string;
+
+  @ApiProperty({
+    example : "1111",
+    description : "가입할 유저의 비밀번호",
+    required : true
+    })
+  @IsNotEmpty()
+  @IsString()
+  password : string;
+  
+  @ApiProperty({
+    example : "testKing",
+    description : "가입할 유저의 닉네임",
+    required : true
+    })
+  @IsNotEmpty()
+  nickname : string;
+}
